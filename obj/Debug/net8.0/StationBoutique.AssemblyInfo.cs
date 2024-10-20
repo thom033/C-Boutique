a3d7b02ede58971c6756bd7988aa8d9864a65984
MialitianaFenosoa <tsantarandriamihary@gmail.com>
2024-10-20 23:10:43 +0300

    Liste Vente

--- obj/Debug/net8.0/StationBoutique.AssemblyInfo.cs
+++ obj/Debug/net8.0/StationBoutique.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StationBoutique")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2211cf3b2ae0a89a7ee41167d99dc63c397fca16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40c2c07fc91ceb235fd95b20dd5af60a0cbba771")]
 [assembly: System.Reflection.AssemblyProductAttribute("StationBoutique")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StationBoutique")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
